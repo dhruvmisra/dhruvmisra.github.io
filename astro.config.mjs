a29ef04d87d02d2e13266288d2729e0b8cdd0e53
dhruvmisra <dhruvmisra@live.com>
2023-05-14 02:18:43 +0530

    Add copy button to code blocks and upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,56 @@ import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import { astroImageTools } from "astro-imagetools";
 import rehypePrettyCode from "rehype-pretty-code";
+import { visit } from "unist-util-visit";
 
 const prettyCodeOptions = {
     // Shiki theme
-    theme: "material-palenight",
+    theme: "material-theme-palenight",
+
     onVisitLine(node) {
         if (node.children.length === 0) {
-            node.children = [{ type: "text", value: " " }];
+            node.children = [
+                {
+                    type: "text",
+                    value: " ",
+                },
+            ];
         }
     },
+
     onVisitHighlightedLine(node) {
         node.properties.className.push("highlighted");
     },
+
     onVisitHighlightedWord(node) {
         node.properties.className = ["word"];
     },
+};
+
+const addRawCodeToMetaNode = () => (tree) => {
+    visit(tree, (node) => {
+        if (node?.type === "element" && node?.tagName === "pre") {
+            const [codeEl] = node.children;
+            if (codeEl.tagName !== "code") return;
+            node.raw = codeEl.children?.[0].value;
+        }
+    });
+};
+
+const addRawCodeToChildProperties = () => (tree) => {
+    visit(tree, (node) => {
+        if (node?.type === "element" && node?.tagName === "div") {
+            if (!("data-rehype-pretty-code-fragment" in node.properties)) {
+                return;
+            }
+
+            for (const child of node.children) {
+                if (child.tagName === "pre") {
+                    child.properties["raw"] = node.raw;
+                }
+            }
+        }
+    });
 };
 
 // https://astro.build/config
@@ -29,7 +64,7 @@ export default defineConfig({
     site: "https://example.com",
     markdown: {
         syntaxHighlight: false,
-        rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+        rehypePlugins: [addRawCodeToMetaNode, [rehypePrettyCode, prettyCodeOptions], addRawCodeToChildProperties],
         extendDefaultPlugins: true,
     },
     integrations: [preact(), mdx(), sitemap(), tailwind(), astroImageTools, compress(), robotsTxt()],
---
+++ src/components/CodeBlock/CopyButton.tsx
@@ -0,0 +1,47 @@
+import { useState } from "preact/hooks";
+
+type CopyButtonProps = {
+    text: string;
+};
+
+const CopyButton = ({ text }: CopyButtonProps) => {
+    const [isCopied, setIsCopied] = useState(false);
+
+    const copy = async () => {
+        await navigator.clipboard.writeText(text);
+        setIsCopied(true);
+
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 5000);
+    };
+
+    return (
+        <button
+            class="copy-button p-2 rounded-md border border-slate-500 transition-opacity"
+            title="Copy"
+            onClick={copy}
+        >
+            {isCopied ? (
+                <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#00FF00">
+                    <path d="M0 0h24v24H0z" fill="none" />
+                    <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
+                </svg>
+            ) : (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="18px"
+                    viewBox="0 0 24 24"
+                    width="18px"
+                    class="fill-slate-400"
+                    fill="currentColor"
+                >
+                    <path d="M0 0h24v24H0z" fill="none" />
+                    <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+                </svg>
+            )}
+        </button>
+    );
+};
+
+export default CopyButton;
