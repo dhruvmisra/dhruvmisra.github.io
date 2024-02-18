9f6a35affcbcd2bd1c19feacace8d0bfca5727ba
dhruvmisra <dhruvmisra@live.com>
2024-02-18 16:06:31 +0530

    feat: init project using astro-paper

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
-    extends: ["plugin:astro/recommended"],
-    overrides: [
-        {
-            files: ["*.astro"],
-            parser: "astro-eslint-parser",
-            parserOptions: {
-                parser: "@typescript-eslint/parser",
-                extraFileExtensions: [".astro"],
-            },
-            rules: {},
-        },
-    ],
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: ["eslint:recommended", "plugin:astro/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
 };
